cb30dd0aa25c12d36865105a17abeebfb5061d27
Brandon Olivares <bolivares@oomphinc.com>
2019-11-24 11:47:10 -0500

    Create Account and AccountType entities

---
+++ src/Entity/Account.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AccountRepository")
+ */
+class Account
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\AccountType")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?AccountType
+    {
+        return $this->type;
+    }
+
+    public function setType(?AccountType $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
