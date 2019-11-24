3a3fe9696e7636baa386ec7d3f0df59a59f84020
Brandon Olivares <bolivares@oomphinc.com>
2019-11-24 16:45:25 -0500

    Add allocation type field to account

--- src/Entity/Account.php
+++ src/Entity/Account.php
@@ -3,12 +3,18 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AccountRepository")
  */
 class Account
 {
+    // Allocation constants.
+    const ALLOCATION_COST = 'cost';
+    const ALLOCATION_VALUE = 'value';
+    const ALLOCATION_TYPES = [self::ALLOCATION_COST, self::ALLOCATION_VALUE];
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -26,6 +32,12 @@ class Account
      * @ORM\JoinColumn(nullable=false)
      */
     private $type;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     * @Assert\Choice(choices=Account::ALLOCATION_TYPES, message="Choose a valid allocation type")
+     */
+    private $allocationType = self::ALLOCATION_COST;
 
     public function getId(): ?int
     {
@@ -52,6 +64,18 @@ class Account
     public function setType(?AccountType $type): self
     {
         $this->type = $type;
+
+        return $this;
+    }
+
+    public function getAllocationType(): ?string
+    {
+        return $this->allocationType;
+    }
+
+    public function setAllocationType(string $allocationType): self
+    {
+        $this->allocationType = $allocationType;
 
         return $this;
     }
