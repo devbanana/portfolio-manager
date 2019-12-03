aa4e96b392d40f019079b25605125cbf546aab45
Brandon Olivares <bolivares@oomphinc.com>
2019-12-03 00:04:53 -0500

    Modify migration to insert default data

--- src/Migrations/Version20191203042913.php
+++ src/Migrations/Version20191203042913.php
@@ -24,6 +24,30 @@ final class Version20191203042913 extends AbstractMigration
         $this->addSql('ALTER TABLE portfolio_holding ADD CONSTRAINT FK_D0AF7D8B96B5643 FOREIGN KEY (portfolio_id) REFERENCES portfolio (id)');
         $this->addSql('ALTER TABLE account_holding RENAME INDEX idx_5bbfd8165da1941 TO IDX_9294CC5E5DA1941');
         $this->addSql('ALTER TABLE account_holding RENAME INDEX idx_5bbfd8169b6b5fba TO IDX_9294CC5E9B6B5FBA');
+
+        // Insert Cash Reserve and default to 5%
+        $this->addSql('INSERT INTO portfolio (name, cash_reserve, allocation_percent) VALUES("Cash Reserve", TRUE, 0.05)');
+
+        // Cash Reserve Portfolio should have Cash asset
+        $this->addSql(
+            'INSERT INTO portfolio_holding ' .
+            '(asset_id, portfolio_id, shares, total_value, invested) VALUES(' .
+            "(SELECT id FROM asset WHERE symbol = '$' ORDER BY id LIMIT 1), " .
+            "(SELECT id FROM PORTFOLIO WHERE cash_reserve IS TRUE ORDER BY id LIMIT 1), " .
+            '0, 0, 0)'
+        );
+
+        // Create default investing portfolio
+        $this->addSql('INSERT INTO portfolio (name, cash_reserve, allocation_percent) VALUES("Stocks", FALSE, NULL)');
+
+        // Stocks portfolio should also have Cash
+        $this->addSql(
+            'INSERT INTO portfolio_holding ' .
+            '(asset_id, portfolio_id, shares, total_value, invested) VALUES(' .
+            "(SELECT id FROM asset WHERE symbol = '$' ORDER BY id LIMIT 1), " .
+            "(SELECT id FROM PORTFOLIO WHERE name = 'Stocks' ORDER BY id LIMIT 1), " .
+            '0, 0, 0)'
+        );
     }
 
     public function down(Schema $schema) : void
