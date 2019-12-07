1e741ee13bffe42f2cbc4bf6c3c7b8fe1006e20c
Brandon Olivares <bolivares@oomphinc.com>
2019-12-07 17:51:22 -0500

    Add unique constraint on portfolio name

---
+++ src/Migrations/Version20191207224228.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20191207224228 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Make portfolio name unique';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_A9ED10625E237E06 ON portfolio (name)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_A9ED10625E237E06 ON portfolio');
+    }
+}
